0c0c6ec077e0bef25d4327604de293ff1ffcf587
João Pedro Souza <jsouza.1650@gmail.com>
2022-04-03 20:36:53 -0300

    init commit

---
+++ PostalCodeDataGetter/PostalCodeDataGetter.cpp
@@ -0,0 +1,52 @@
+// PostalCodeDataGetter.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+CURL* curl;
+CURLcode res;
+std::string readBuffer;
+
+std::string postalCode;
+std::string url;
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+int main()
+{
+    std::cout << "Please inform your postal code." << std::endl;
+
+    std::cin >> postalCode;
+
+    url = "https://viacep.com.br/ws/" + postalCode + "/json";
+
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+        res = curl_easy_perform(curl);
+        
+        curl_easy_cleanup(curl);
+
+        std::cout << readBuffer << std::endl;
+    }
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
